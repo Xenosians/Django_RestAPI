b947616eaea35a8bf1c6da4f10298f819736760b
Xenosians <fio.salis@gmail.com>
2021-04-21 22:04:15 +0700

    Adding Login Logout Function

--- makanan/settings.py
+++ makanan/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'RestAPI',
     'rest_framework',
+    "knox",
+    "Account",
 ]
 
 MIDDLEWARE = [
@@ -132,3 +134,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'knox.auth.TokenAuthentication',
+    ]
+}
--- makanan/urls.py
+++ makanan/urls.py
@@ -19,4 +19,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('API/',include('RestAPI.urls')),
+    path('API/',include('Account.urls')),
 ]
